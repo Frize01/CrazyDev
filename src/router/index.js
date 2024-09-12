51b202e9cf5e32e0f16659788b9d580c79a7ac6d
Kévin <voisine.kevin2@gmail.com>
2024-09-12 09:35:54 +0200

    [feat] Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import GameView from '@/views/GameView.vue'
+import InstuctionsView from '@/views/InstuctionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: GameView
+    },
+    {
+      path: '/instruction',
+      name: 'instruction',
+      component: InstuctionsView
     }
   ]
 })
