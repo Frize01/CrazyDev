b98ae9777384ef04fa20c019314adbe310becfb5
Kévin <voisine.kevin2@gmail.com>
2024-09-13 15:47:46 +0200

    [feat] PWA

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,29 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    VitePWA({
+      manifest: {
+        name: 'Digital Student',
+        short_name: 'Digital Student',
+        icons: [
+          {
+            src: 'icons/1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
